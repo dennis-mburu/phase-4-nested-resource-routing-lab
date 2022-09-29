c1a4f1c991d9e10b4931ba6d02466f3505f104e8
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-09-29 21:19:55 +0300

    Assignment Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: user.items, include: :user
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: 201
+  end
+
+  private 
+  
+  def render_not_found
+    render json: {error: "Not Found"}, status: 404
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
